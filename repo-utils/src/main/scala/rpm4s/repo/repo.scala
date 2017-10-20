eeb6c5383d1fcef09e7d95062bcebb2a06751269
Kevin Walter <kevin.walter.private@gmail.com>
2017-10-21 00:20:18 +0200

    change repo.create to accept any F[_] with a Sync

--- repo-utils/src/main/scala/rpm4s/repo/repo.scala
+++ repo-utils/src/main/scala/rpm4s/repo/repo.scala
@@ -4,29 +4,30 @@ import java.nio.charset.StandardCharsets
 import java.nio.file._
 import java.security.MessageDigest
 
-import cats.effect.IO
+import cats.effect.{Sync}
 import cats.implicits._
 import fs2.{Chunk, Sink, Stream}
 import rpm4s.data.Checksum.Sha256
 import rpm4s.data._
 import rpm4s.repo.utils.compress.gzip
+import cats.implicits._
 
 package object repo {
 
-  def create(
+  def create[F[_]: Sync](
     reporoot: Path,
-    rpms: Stream[IO, (RpmPrimaryEntry, Checksum)],
+    rpms: Stream[F, (RpmPrimaryEntry, Checksum)],
     nameFn: (RpmPrimaryEntry, Checksum) => String,
     revision: Long,
     count: Option[Long]
-  ): IO[Unit] = {
+  ): F[Unit] = {
     val primaryFile = reporoot.resolve("primary.xml.gz")
     val repomdFile = reporoot.resolve("repomd.xml")
     Files.deleteIfExists(primaryFile)
     Files.deleteIfExists(repomdFile)
     create(
-      fs2.io.file.writeAll[IO](repomdFile),
-      fs2.io.file.writeAll[IO](primaryFile),
+      fs2.io.file.writeAll(repomdFile),
+      fs2.io.file.writeAll(primaryFile),
       nameFn,
       rpms,
       revision,
@@ -34,15 +35,15 @@ package object repo {
     )
   }
 
-  def create(
-    repomdSink: Sink[IO, Byte],
-    primarySink: Sink[IO, Byte],
+  def create[F[_]: Sync](
+    repomdSink: Sink[F, Byte],
+    primarySink: Sink[F, Byte],
     nameFn: (RpmPrimaryEntry, Checksum) => String,
-    rpms: Stream[IO, (RpmPrimaryEntry, Checksum)],
+    rpms: Stream[F, (RpmPrimaryEntry, Checksum)],
     revision: Long,
     count: Option[Long]
-  ): IO[Unit] = {
-    rpm4s.repo.utils.hash.checksumsBeforeAndAfter[IO](
+  ): F[Unit] = {
+    rpm4s.repo.utils.hash.checksumsBeforeAndAfter(
       repomd.xml.primary.create(count, rpms, nameFn).through(fs2.text.utf8Encode),
       gzip(),
       MessageDigest.getInstance("SHA-256"),
@@ -53,7 +54,7 @@ package object repo {
         val repomdBytes =
           repomd.create(gzSize, openSize, gzChecksum, openChecksum, revision)(
             new StringBuilder).toString.getBytes(StandardCharsets.UTF_8)
-        Stream.chunk(Chunk.bytes(repomdBytes)).covary[IO].to(repomdSink).run
+        Stream.chunk(Chunk.bytes(repomdBytes)).covary[F].to(repomdSink).run
     }
   }
 
