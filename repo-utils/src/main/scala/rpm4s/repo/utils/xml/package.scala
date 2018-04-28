28516e38867f4f899e17177ac08b522323f4db72
Kevin Walter <kevin.walter.private@gmail.com>
2018-04-28 12:38:15 +0200

    fix xml imports for java 9 and later

--- repo-utils/src/main/scala/rpm4s/repo/utils/xml/package.scala
+++ repo-utils/src/main/scala/rpm4s/repo/utils/xml/package.scala
@@ -1,11 +1,12 @@
 package rpm4s.repo.utils
 
 import java.io.InputStream
+
 import javax.xml.stream.{XMLInputFactory, XMLStreamReader}
 import javax.xml.stream.events.{EndElement, StartElement, XMLEvent}
-
+import javax.xml.stream.XMLInputFactory
+import javax.xml.stream.util.XMLEventAllocator
 import cats.effect.Sync
-import com.sun.xml.internal.fastinfoset.stax.factory.StAXInputFactory
 import com.sun.xml.internal.stream.events.XMLEventAllocatorImpl
 import fs2.{Pipe, Stream}
 
@@ -32,14 +33,14 @@ package object xml {
   }
 
   def xmlreader[F[_]]: Pipe[F, InputStream, XMLStreamReader] = _.map { in =>
-    val xmlif = StAXInputFactory.newInstance()
+    val xmlif = XMLInputFactory.newInstance()
     xmlif.setProperty(XMLInputFactory.SUPPORT_DTD, false); // This disables DTDs entirely for that factory
     xmlif.setProperty("javax.xml.stream.isSupportingExternalEntities", false); // disable external entities
     xmlif.createXMLStreamReader(in)
   }
 
   def xmlevents[F[_]: Sync](in: InputStream): Stream[F, XMLEvent] = {
-    val xmlif = StAXInputFactory.newInstance()
+    val xmlif = XMLInputFactory.newInstance()
     xmlif.setEventAllocator(new XMLEventAllocatorImpl())
     Stream.bracket(Sync[F].delay(xmlif.createXMLStreamReader(in)))(
       xmlsr => {
