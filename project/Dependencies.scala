9ff2b6f0741b2a8fa2dde40d5cd156dbaeee7cd0
Scala steward <me@scala-steward.org>
2019-01-05 08:30:47 +0100

    Update circe-core, circe-generic, ... to 0.9.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   val scalatest = "3.0.0"
   val scalacheck = "1.13.5"
   val http4s = "0.18.0"
-  val circe = "0.9.1"
+  val circe = "0.9.3"
   val shapeless = "2.3.3"
   val scodecCore = "1.10.3"
 }
