8200e8145f908f064501ad9da86b58152d6478f8
Kevin Walter <kevin.walter.private@gmail.com>
2020-01-02 20:28:59 +0100

    add more information to assertions

--- cli/src/main/scala/rpm4s/cli/Main.scala
+++ cli/src/main/scala/rpm4s/cli/Main.scala
@@ -16,7 +16,7 @@ case class RpmInfo(
  architecture: Architecture,
  vendor: Vendor,
  license: License,
- summery: Summery,
+ summery: Summary,
  description: Description,
  group: rpm4s.data.Group,
  compression: Compression,
