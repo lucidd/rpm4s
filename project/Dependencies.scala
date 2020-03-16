f6d288a788f91ccc041492c8f34f7e0aa839a93b
Scala Steward <me@scala-steward.org>
2020-03-17 00:23:05 +0100

    Update scala-xml to 1.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val circe = "0.13.0"
   val shapeless = "2.3.3"
   val scodecCore = "1.11.4"
-  val scalaXML = "1.2.0"
+  val scalaXML = "1.3.0"
   val decline = "1.0.0"
   val commonsCompress = "1.19"
   val betterFiles = "3.6.0"
