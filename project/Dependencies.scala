607a4b19e1a927231b04dc8420f194ead9908aeb
Scala Steward <me@scala-steward.org>
2021-03-29 13:24:08 +0200

    Update http4s-blaze-client, ... to 0.21.21

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   val scalatest = "3.2.2"
   val scalatestplus = "3.2.2.0"
   val scalacheck = "1.14.3"
-  val http4s = "0.21.7"
+  val http4s = "0.21.21"
   val circe = "0.13.0"
   val shapeless = "2.3.3"
   val scodecCore = "1.11.4"
